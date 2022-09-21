3845224d1767a26ce66de39247e2a41865283ef1
lami_lekan <olagbajuisrael11@gmail.com>
2022-09-21 08:10:14 +0000

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FIzzBizz");
+			printf(" FizzBizz");
 		}
 		else if (n % 3 == 0)
 		{
@@ -22,7 +22,7 @@ int main(void)
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
 		}
 		else
 		{
