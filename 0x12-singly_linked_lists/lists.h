4158c7840a822f2a45fafe1308da8d8d0b2f5739
lami-lekan <110998604+lami-lekan@users.noreply.github.com>
2022-10-24 16:14:33 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,20 +5,20 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed strings)
-* @len: length of the string
-* @next: points to the next node
-*
-* Description: All prototypes used in the singly linked directory
-* are contained her and is part of the necessities for succesfull
-* program compilation.
-*/
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: All prototypes used in the singly linked directory
+ * are contained here and is part of the necessities for succesfull
+ * program compilation.
+ */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
