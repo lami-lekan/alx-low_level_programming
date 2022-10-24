b1d8ccb0999c584fc82b79bf18da3b9991628de7
lami-lekan <110998604+lami-lekan@users.noreply.github.com>
2022-10-24 16:15:23 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
-* print_list - prints list.
-* @h: param list
-* Return: struct
-*/
+ * print_list - prints list.
+ * @h: param list
+ * Return: struct
+ */
 size_t print_list(const list_t *h)
 {
 	size_t nodelen;
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nill)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
