5cb5e5ade77a2aab34e7e45c5b4fa5f47efd781a
lami-lekan <110998604+lami-lekan@users.noreply.github.com>
2022-12-14 21:51:33 +0100

    Update 5-strstr.c

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -1,35 +1,30 @@
 #include "main.h"
 
 /**
- * _strstr - finds the first occurrence of the substring needle
- *  in the string haystack
- *
- * @haystack: string to work on
- * @needle: substring to match
- * Return: pointer to the first match or NULL
- */
-char *_strstr(char *haystack, char *needle)
-{
-	int i, j, match;
+ * _strstr - first occurrence of the substring needle in the string haystack
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ **/
 
-	if (*needle == '\0')
-		return (haystack);
-	for (i = 0; haystack[i] != '\0'; i++)
+char  *_strstr(char *haystack, char *needle)
+{
+	char *str1, *str2; /*Declaring variables*/
+
+	while (*haystack != '\0')
 	{
-		if (haystack[i] == *needle)
+		str1 = haystack; /*values*/
+		str2 = needle;
+
+		/*Star WHILE*/
+		while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
 		{
-			for (j = 1; needle[j] != '\0'; j++)
-			{
-				if (needle[j] != haystack[i + j])
-				{
-					match = 0;
-					break;
-				}
-				match = 1;
-			}
-			if (match)
-				return (haystack + i);
+			haystack++;
+			str2++;
 		}
+		if (*str2 == '\0')
+			return (str1);
+		haystack = str1 + 1;
 	}
-	return (NULL);
+	return (0);
 }
