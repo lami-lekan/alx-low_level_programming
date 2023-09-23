9f0d3e6c7e67f03491e2d6b2f29e91cc2b1c7395
lami_lekan <olagbajuisrael11@gmail.com>
2023-09-23 18:43:20 +0000

    Ascii use

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main -entry line
+*
+* Return: 0
+*/
+int main(void)
+{
+int a = 97;
+while (a < 123)
+{
+	putchar(a);
+	a++;
+	}
+	putchar(10);
+	return (0);
+}
