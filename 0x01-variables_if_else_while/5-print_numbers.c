dbd8050292a46d35b51f94f82b3432c3861354e5
lami_lekan <olagbajuisrael11@gmail.com>
2023-09-25 12:37:28 +0000

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%d", digit);
+		digit++;
+		}
+
+	return (0);
+	}
