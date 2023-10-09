fc78423a25d13837a32a85b3bceb26863b0a95ab
lami_lekan <olagbajuisrael11@gmail.com>
2023-10-09 01:02:31 +0000

    Printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+*
+* Return: 0
+*/
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+		}
+	
+	putchar(10);
+	return (0);
+	}
