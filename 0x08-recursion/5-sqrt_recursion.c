b6497f30e980d9edde296e1acb5c76ff57a38d02
lami_lekan <olagbajuisrael11@gmail.com>
2023-10-23 15:52:37 +0000

    Recurse sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
-* _sqrt_recursion - gives sqrt of natural numbers
-*
-* _sqrt - gives roots and return an int
+* _sqrt_1 - gives roots and return an int
 *
 * @test: a first test
 * @n: int to work on
 * @n: int
 * Return: int
 */
-int _sqrt(int n, int test)
+int _sqrt_1(int n, int test)
 {
 	if ((test * test) == n)
 	{
@@ -17,12 +15,18 @@ int _sqrt(int n, int test)
 	}
 	else if ((test * test) < n)
 	{
-		return (_sqrt(n, test + 1));
+		return (_sqrt_1(n, test + 1));
 	}
 	else if ((test * test) > n)
 		return (-1);
 	return (0);
 }
+/**
+* _sqrt_recursion - gives sqrt of natural int
+*
+* @n: int
+*Return: int
+*/
 
 int _sqrt_recursion(int n)
 {
@@ -30,7 +34,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt(n, 2));
+	return (_sqrt_1(n, 2));
 }
 
 
